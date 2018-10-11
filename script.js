b31c67de56a56df34f4e5b2c8dbede49d9a48824
ArkadiuszBlochowiak <blochowiak.dev@gmail.com>
2018-10-11 21:25:15 +0200

    Generating a JSON objects

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 function zespoly() {
-	var dane = document.getElementById("teams2").value;
+	var dane = document.getElementById("teams").value;
 	if(dane.value != "")
 	{
 		var array = dane.split(",");
 		for(var i=0;i<array.length;i++)
 		{
-			document.getElementById("dane2").innerHTML += array[i]+"<br/>";
+			document.getElementById("dane").innerHTML += array[i]+"<br/>";
 		}
 	}
-	document.getElementById("dane2").innerHTML += "<br/><br/><br/>";
+	document.getElementById("dane").innerHTML += "<br/><br/><br/>";
 	
 	var arrayJSON = JSON.stringify(array);
-	document.getElementById("dane2").innerHTML += arrayJSON;
+	document.getElementById("dane").innerHTML += arrayJSON;
+	
+	var teams=[];
+	var zmienna = 1;
+	var sprawdzenie = false;
+	for(var i=0;i<array.length;i++)
+	{
+		
+		teams[i] = {"name":array[i],"eliminated":"false","match":zmienna};
+		console.log(teams[i]);
+		if(sprawdzenie==false)
+		{
+			sprawdzenie=true;
+		}
+		else
+		{
+			sprawdzenie=false;
+			zmienna++;
+		}
+	}
+	document.getElementById("dane").innerHTML += "<br/><br/><br/>";
+	document.getElementById("dane").innerHTML += teams[0].name;
+	
+	
+	
 }
+/*
+`${}
+
+var teams = [
+{
+	"name":"dfsdf"
+	"eliminated":"false"
+	"match":"1"
+}]
+*/
