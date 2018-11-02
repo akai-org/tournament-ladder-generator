e6678e25dd31e105e8589218ec133ea3bafe578e
ArkadiuszBlochowiak <blochowiak.dev@gmail.com>
2018-11-02 16:40:59 +0100

    Poprawne przechodzenie dla 4,8,16 itd. drużyn

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 var teams=[];
-//-----------------funkcja losujaca ---------------  B - tablica pomocnicza
-function draw(teams) {
-	let B=[];
-	B=teams;
-	teams=[];
-	console.log("rand");
-	let position ;
-	while (B.length!=0) {
-	let position = Math.floor(Math.random()*B.length);
-	let team=B[position];
-	teams.push(team);
-	B.splice(position, 1)
-	}
-	return teams;
-}
 
-//-
-function main() {
+function main() {	// główna funkcja
 	var data = document.getElementById("teams").value;
-	data="Lech Poznan,Legia Warszawa, Wisła Kraków, Lechia Gdańsk";
+	data="Lech Poznan,Legia Warszawa, Wisła Kraków, Lechia Gdańsk, Pogoń Szczecin, Arka Gdynia, Zaglebie Lublin, Gornik Zabrze";
 
 	if(data.value != "")
 	{
@@ -30,8 +14,7 @@ function main() {
 		array[i] = array[i].trim();
 	}
 
-	array = draw(array);	//funkcja losująca
-
+	array = draw(array);
 
 	var number = 1;
 	var check = false;
@@ -49,19 +32,33 @@ function main() {
 			number++;
 		}
 	}
-
+	
 	console.log(teams);
-	draw_div(); // wywoalnie funkcji torzenie div-ow
+	draw_div(); 
+}
+
+function draw(teams) {	//funkcja losujaca
+	let B=[];
+	B=teams;
+	teams=[];
+	let position ;
+	while (B.length!=0) {
+		let position = Math.floor(Math.random()*B.length);
+		let team=B[position];
+		teams.push(team);
+		B.splice(position, 1)
+	}
+	return teams;
 }
 
 function transition(object,game)	//rozdzielanie do roznych funkcji
 {
 	let win_id = object.id;
 	let win_text = object.getAttribute("data-name");
-	console.log(win_text);
+	console.log("game: "+game);
+	
 	let lose_text;
-	let index;
-
+	let lose_i,win_i;
 	for(let i=0;i<teams.length;i++)
 	{
 		if(teams[i].match == game)
@@ -69,16 +66,22 @@ function transition(object,game)	//rozdzielanie do roznych funkcji
 			if(teams[i].name != win_text)
 			{
 				lose_text = teams[i].name;
-				index=i;
-				break;
+				lose_i=i;
+			}
+			else
+			{
+				win_i=i;
 			}
 		}
 	}
-
+	console.log("Druzyna ktora przegrala: "+lose_text);
 	let lose_id = lose_text.split(' ').join('_');
-
-	lose(lose_id,index);
-	win(win_id,win_text);
+	//console.log("Index druzyny ktora przegrala: "+lose_i);
+	//console.log("Index druzyny ktora wygrala: "+win_i);
+	console.log(object);
+	
+	lose(lose_id,lose_i);
+	win(win_id,win_text,game,win_i);
 }
 
 function lose(id,index)	// wyszarzanie przegranych
@@ -90,19 +93,88 @@ function lose(id,index)	// wyszarzanie przegranych
 	lose.classList.add("team-grayed-out");
 }
 
-function win(id,txt)	//przechodzenie do kolejnego etapu
+function win(id,txt,game,index)	//przechodzenie do kolejnego etapu
 {
 	let win = document.getElementById(id);
 	win.innerHTML="&#x2714";
-	alert("Dalej przechodzi druzyna "+txt);
+	win.removeAttribute("id");
+	let sum=eliminated_check();
+	
+	if(sum!=1)
+	{
+		let p1 = next_round_number(game); // numer potrzebny do identyfikacji meczu
+		let p2 = next_round_tb(game);// sprawdza czy ma byc top czy bottom
+		let p = `${p2}_${p1}`;
+		console.log("p1: "+p1);
+		let div = document.getElementById(p).children;
+		
+		div[0].innerHTML = txt; //div
+		div[1].id = id; //przycisk
+		div[1].setAttribute(`onclick`,`transition(this,${p1+1})`);
+		div[1].setAttribute(`data-name`,`${txt}`)
+		
+		teams[index].match=p1+1;
+	}
+	else
+	{
+		alert("Turniej wygrała "+txt);
+	}
 }
 
-function draw_div(){
+function next_round_number(game)
+{
+	//console.log(teams.length/2+game-1-temp_array(game-1));
+	return teams.length/2+game-1-temp_array(game-1); //wzor: ilsc druzyn + index meczu - watrosc tablicy[index meczu]
+}
+
+function temp_array(value) 	//określenie ile miejsc ma sie przesuwać dana "druzyna"
+{
+	let a=[];
+	a[0]=0;
+	let num=1;
+	let check=false;
+	for(let i=1;i<value+1;i++)
+	{
+		a[i]=num;
+		if(check==false)
+		{
+			check=true;
+		}
+		else
+		{
+			check=false;
+			num++;
+		}
+	}
+	
+	return a[value];
+}
+
+function next_round_tb(game) 	//okreslanie czy dana druznyna ma przejsc do top czy bottom
+{
+	if(game%2!=0)
+		return 'top';
+	else
+		return 'bottom';
+}
+
+function eliminated_check()		//funkcja sprawdzajaca ile druzyn zostalo
+{
+	let sum=0;
+	for(let i=0;i<teams.length;i++)
+	{
+		if(teams[i].eliminated == 'false')
+			sum++;
+	}
+	return sum;
+}
+
+function draw_div(){	// funkcja torzenia div-ow
 	let round_amount = Math.ceil(Math.log2(teams.length));
-	let container=document.getElementById("ladder"); //główny div
+	let container=document.getElementById("ladder"); 	//główny div
 	let squad=0;
-	let teams_amount = teams.length/2;
-	let mnr=0; //numer meczu
+	let teams_amount = teams.length/2;	//liczba meczy w danej rundzie
+	let mnr=0; 	//numer meczu
 	for(let i=0;i<round_amount;i++)
 	{
 		let newRound=document.createElement("div");
@@ -112,18 +184,18 @@ function draw_div(){
 		
 		for(let i=0;i<teams_amount;i++)
 		{
-			let newTop=document.createElement("div")
+			let newTop=document.createElement("div");	//tworzenie	diva top
 			newTop.setAttribute(`class` , `top`);
 			newTop.setAttribute(`id`,`top_${mnr}`);
 			
-			let newBottom=document.createElement("div")
+			let newBottom=document.createElement("div")		//tworzenie diva bottom
 			newBottom.setAttribute(`class` , `bottom`);
 			newBottom.setAttribute(`id`,`bottom_${mnr}`);
 
-			let newMatch=document.createElement("div"); //toworzenie diva
-			newMatch.setAttribute(`id` , `${"match_"}${mnr}`); // nadanie id meczu
+			let newMatch=document.createElement("div"); 	//tworzenie diva meczu
+			newMatch.setAttribute(`id` , `${"match_"}${mnr}`); 	
 			newMatch.setAttribute(`class` , `match`);
-			container.appendChild(newMatch); //dodanie do glownego diva
+			container.appendChild(newMatch); 	//dodanie do glownego diva
 
 			newMatch.appendChild(newTop);
 			newMatch.appendChild(newBottom);
@@ -131,15 +203,16 @@ function draw_div(){
 			
 			for(let j=0;j<2;j++)
 			{
-				let newTeam=document.createElement("div");
+				let newTeam=document.createElement("div");		//tworzenie diva druzyny
 				newTeam.setAttribute(`class` , `team`);
 				
-				let btn = document.createElement("button");
+				let btn = document.createElement("button");		//tworzenie przycisku druzyny
+				btn.textContent="-";
 				
-				if(squad<=teams.length)
+				if(squad<teams.length)
 				{
 					newTeam.textContent=teams[squad].name;
-					newTeam.setAttribute(`id` , `${"team_"}${squad}` );
+					//newTeam.setAttribute(`id` , `${"team_"}${squad}` );
 					
 					let identyfier = teams[squad].name.split(' ').join('_');
 					let game = teams[squad].match;
@@ -147,10 +220,9 @@ function draw_div(){
 					btn.setAttribute(`onclick`,`transition(this,${game})`);
 					btn.setAttribute(`id`,identyfier);
 					btn.setAttribute(`data-name`,`${teams[squad].name}`);
-					btn.textContent="-";
 				}
 
-				if(j==0)
+				if(j==0)	//przypisywanie do top albo bottom
 				{
 					newTop.appendChild(newTeam);
 					newTop.appendChild(btn);
@@ -162,67 +234,8 @@ function draw_div(){
 				}
 				squad++;
 			}
-			
 			mnr++;
 		}
 		teams_amount=teams_amount/2;
 	}
-
 }
-
-/*
-function draw_div(){ 	// funkcja tworzenie div-------------
-	let round_amount=Math.ceil(Math.log2(teams.length)); // ilosc rund
-	let container=document.getElementById("ladder");
-	console.log(round_amount);
-	let squad=0;
-	for(let i=0;i<round_amount;i++){ // petla tworzaca diva mecz
-
-//----------------div top--------------------------
-		let newTop=document.createElement("div")
-		newTop.setAttribute(`class` , `top`);
-
-/ --------------------div bottom--------------------- 
-		let newBottom=document.createElement("div")
-		newBottom.setAttribute(`class` , `bottom`);
-
-		let newMatch=document.createElement("div"); //toworzenie diva
-		newMatch.setAttribute(`id` , `${"match_"}${i}`); // nadanie id meczu
-		newMatch.setAttribute(`class` , `match`);
-		container.appendChild(newMatch); //dodanie do glownego diva
-
-		newMatch.appendChild(newTop);
-		newMatch.appendChild(newBottom);
-
-//--------------------------------------------------------------------------------------------
-		for(let i=0;i<2;i++)
-		{
-			let newTeam=document.createElement("div")
-			newTeam.textContent=teams[squad].name;
-			newTeam.setAttribute(`id` , `${"team_"}${squad}` );
-			newTeam.setAttribute(`class` , `team`);
-
-			let identyfier = teams[squad].name.split(' ').join('_');
-			let game = teams[squad].match;
-
-			let btn = document.createElement("button");
-			btn.setAttribute(`id`,identyfier);
-			btn.setAttribute(`onclick`,`transition(this,${game})`);
-			btn.textContent=teams[squad].name;
-
-			if(i==0)
-			{
-				newTop.appendChild(newTeam);
-				newTop.appendChild(btn);
-			}
-			else
-			{
-				newBottom.appendChild(newTeam);
-				newBottom.appendChild(btn);
-			}
-			squad++;
-		}
-	}
-	draw_empty_div()
-}
-*/
