a54659b38aac9af768d22dcc9f63e87798553304
Dudek191294 <d.dudziak191294@gmail.com>
2018-12-08 16:24:44 +0100

    skyrp develop

--- script.js
+++ script.js
@@ -37,45 +37,6 @@ function draw(teams) {
   return teams;
 }
 
-<<<<<<< HEAD
-//-
-function main() {
-	var data = document.getElementById("teams").value;
-	data="Lech Poznan,Legia Warszawa, Wisła Kraków, Lechia Gdańsk";
-
-	if(data.value != "")
-	{
-		var array = data.split(",");
-	}
-
-	for(var i=0;i<array.length;i++)
-	{
-		array[i] = array[i].trim();
-	}
-
-	array = draw(array);	//funkcja losująca
-
-
-	var number = 1;
-	var check = false;
-	for(var i=0;i<array.length;i++)
-	{
-		teams[i] = {name:array[i],eliminated:"false",match:number};
-
-		if(check==false)
-		{
-			check=true;
-		}
-		else
-		{
-			check=false;
-			number++;
-		}
-	}
-
-	console.log(teams);
-	draw_div(); // wywoalnie funkcji torzenie div-ow
-=======
 function transition(object, game) {
   //rozdzielanie do roznych funkcji
   const win_id = object.id;
@@ -185,7 +146,6 @@ function temp_array(value) {
   }
 
   return a[value];
->>>>>>> origin/Daniel
 }
 
 function eliminated_check() {
@@ -239,30 +199,6 @@ function draw_div() {
       newBottom.setAttribute(`class`, `bottom`);
       newBottom.setAttribute(`id`, `bottom_${mnr}`);
 
-<<<<<<< HEAD
-function transition(object,game)	//rozdzielanie do roznych funkcji
-{
-	let win_id = object.id;
-	let win_text = object.innerHTML;
-	let lose_text;
-
-	for(let i=0;i<teams.length;i++)
-	{
-		if(teams[i].match == game)
-		{
-			if(teams[i].name != win_text)
-			{
-				lose_text = teams[i].name;
-			}
-		}
-	}
-
-	let lose_id = lose_text.split(' ').join('_');
-
-
-	lose(lose_id);
-	win(win_id,win_text);
-=======
       const newMatch = document.createElement("div"); //tworzenie diva meczu
       newMatch.setAttribute(`id`, `${"match_"}${mnr}`);
       newMatch.setAttribute(`class`, `match`);
@@ -293,7 +229,6 @@ function transition(object,game)	//rozdzielanie do roznych funkcji
     }
     teams_amount = teams_amount / 2;
   }
->>>>>>> origin/Daniel
 }
 
 function insert() {
@@ -322,73 +257,6 @@ function insert() {
   }
 }
 
-<<<<<<< HEAD
-function draw_div(){ 	// funkcja tworzenie div-------------
-	let round_amount=Math.ceil(Math.log2(teams.length)); // ilosc rund
-	let container=document.getElementById("ladder");
-	console.log(round_amount);
-	let squad=0;
-	for(let i=0;i<round_amount;i++){ // petla tworzaca diva mecz
-
-//----------------div top--------------------------
-		let newTop=document.createElement("div")
-		newTop.setAttribute(`class` , `top`);
-
-/* --------------------div bottom--------------------- */
-		let newBottom=document.createElement("div")
-		newBottom.setAttribute(`class` , `bottom`);
-
-		let newMatch=document.createElement("div"); //toworzenie diva
-		newMatch.setAttribute(`id` , `${"match_"}${i}`); // nadanie id meczu
-		newMatch.setAttribute(`class` , `match`);
-		container.appendChild(newMatch); //dodanie do glownego diva
-
-		newMatch.appendChild(newTop);
-		newMatch.appendChild(newBottom);
-
-
-				let j=0;
-				array.forEach(function (){ // petla tworzaca divy z druzynami
-					if((array[j].match===i+1 )&& (array[j].eliminated="false")){
-							let newTeam=document.createElement("div");
-							newTeam.textContent=array[j].name;
-							newTeam.setAttribute(`id` , `${"team_"}${j}` )
-							newMatch.appendChild(newTeam);
-					}
-					j++
-				})
-
-//--------------------------------------------------------------------------------------------
-		for(let i=0;i<2;i++)
-		{
-			let newTeam=document.createElement("div")
-			newTeam.textContent=teams[squad].name;
-			newTeam.setAttribute(`id` , `${"team_"}${squad}` );
-			newTeam.setAttribute(`class` , `team`);
-
-			let identyfier = teams[squad].name.split(' ').join('_');
-			let game = teams[squad].match;
-
-			let btn = document.createElement("button");
-			btn.setAttribute(`id`,identyfier);
-			btn.setAttribute(`onclick`,`transition(this,${game})`);
-			btn.textContent=teams[squad].name;
-
-			if(i==0)
-			{
-				newTop.appendChild(newTeam);
-				newTop.appendChild(btn);
-			}
-			else
-			{
-				newBottom.appendChild(newTeam);
-				newBottom.appendChild(btn);
-
-			}
-			squad++;
-		}
-	}
-=======
 function fix_teams(a) {
   let number = 10 * round_amount + 1;
   let check = false;
@@ -447,36 +315,4 @@ function special_round(a) {
   }
 
   return x;
->>>>>>> origin/Daniel
-}
-
-
-
-
-
-function draw_empty_div (array){
-	let round_amount=Math.ceil(Math.log2(teams.length)); // ilosc rund
-	let container=document.getElementById("ladder");
-	matchId=	round_amount;
-	round_amount=round_amount/2;
-	teamId=array.length;
-
-	for(let i=0;i<round_amount;i++){ // petla tworzaca diva mecz
-
-		let newTop=document.createElement("div")
-		newTop.setAttribute(`class` , `top`);
-
-/* --------------------div bottom--------------------- */
-		let newBottom=document.createElement("div")
-		newBottom.setAttribute(`class` , `bottom`);
-
-		let newMatch=document.createElement("div"); //toworzenie diva
-		newMatch.setAttribute(`id` , `${matchId}`); // nadanie id meczu
-		newMatch.setAttribute(`class` , `match`);
-		container.appendChild(newMatch); //dodanie do glownego diva
-
-		newMatch.appendChild(newTop);
-		newMatch.appendChild(newBottom);
-
- }
 }
